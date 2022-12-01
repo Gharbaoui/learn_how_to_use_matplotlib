27ff7bb102c25bc1cd03e63fe7c2f2a1dcfde249
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-12-01 21:53:11 +0100

    add legend to differ between each line

--- mpl_demo.py
+++ mpl_demo.py
@@ -15,12 +15,18 @@ def plot_auts_temp(x_axis):
     y_axis = rd.randint(60, size=(10))
     plt.plot(x_axis, y_axis)
 
+def plot_legends():
+    plt.legend(['maroc', 'austr'])
+
+def show_graph():
+    plot_legends()
+    plt.show()
 
 def main():
     x_axis = list(range(0, 10))
     plot_moroco_temp(x_axis)
     plot_auts_temp(x_axis)
-    plt.show()
+    show_graph()
 
 if __name__ == "__main__":
     main()
