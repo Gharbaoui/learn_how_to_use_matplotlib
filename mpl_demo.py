70b537f0bd51f28558867d11b5a96dfa5880441c
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-12-01 21:47:49 +0100

    refactor some code

--- mpl_demo.py
+++ mpl_demo.py
@@ -1,13 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy.random as rd
 
-def main():
-    x_axis = list(range(0, 10))
-    y_axis = rd.randint(100, size=(10))
-    plt.plot(x_axis, y_axis)
+def name_axis():
     plt.xlabel('cache-size')
     plt.ylabel('hate-rate')
     plt.title('cache-statics')
+
+
+def plot_moroco_temp(x_axis):
+    y_axis = rd.randint(100, size=(10))
+    plt.plot(x_axis, y_axis)
+
+
+def main():
+    x_axis = list(range(0, 10))
+    plot_moroco_temp(x_axis)
+
     plt.show()
 
 if __name__ == "__main__":
