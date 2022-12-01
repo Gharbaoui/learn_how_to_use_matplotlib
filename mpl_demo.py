5a0f7288d535413020ab25ea417a49a3040d4a4a
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-12-01 21:50:56 +0100

    add a second graph

--- mpl_demo.py
+++ mpl_demo.py
@@ -11,11 +11,15 @@ def plot_moroco_temp(x_axis):
     y_axis = rd.randint(100, size=(10))
     plt.plot(x_axis, y_axis)
 
+def plot_auts_temp(x_axis):
+    y_axis = rd.randint(60, size=(10))
+    plt.plot(x_axis, y_axis)
+
 
 def main():
     x_axis = list(range(0, 10))
     plot_moroco_temp(x_axis)
-
+    plot_auts_temp(x_axis)
     plt.show()
 
 if __name__ == "__main__":
